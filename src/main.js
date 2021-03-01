fdd4ce64b27b08d6d43e9d6e39cf0998d178a672
Pulkit <pulkitsingh302@gmail.com>
2021-03-01 17:37:56 +0530

    new layout projects scss

--- src/main.js
+++ src/main.js
@@ -1,26 +1,22 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/style.scss'
-import settings from "../data/theme.json";
+import DefaultLayout from "~/layouts/Default.vue";
+import "~/assets/style.scss";
 
-
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
-  head.bodyAttrs = {
-    class: settings.dark_mode ? "dark" : ""
-  };
+  Vue.component("Layout", DefaultLayout);
+
   head.link.push(
     {
-      rel: 'prefetch',
-      href: 'https://fonts.gstatic.com'
+      rel: "prefetch",
+      href: "https://fonts.gstatic.com",
     },
     {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
-    },
-
-  )
+      rel: "stylesheet",
+      href:
+        "https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap",
+    }
+  );
 }
